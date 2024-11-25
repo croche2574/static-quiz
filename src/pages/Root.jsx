c2f19351e1d4c1226c0c401cda3728014e5d496a
Colter Roche <croche2574@floridapoly.edu>
2024-11-24 19:46:33 -0500

    First commit

---
+++ src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import React, { memo } from "react";
+import { useNavigate } from "react-router";
+
+const Header = memo((props) => {
+
+}, [])
+
+const Footer = memo((props) => {
+
+}, [])
+
+export const Root = memo(() => {
+    const navigate = useNavigate()
+
+    
+}, [])
